010415cc432a9b9056b18914670d439e917956c4
Martin Grossmann <mhgrossmann@yahoo.com>
2016-04-05 20:37:35 -0500

    added basic js code to entry.js

--- app/scripts/entry.js
+++ app/scripts/entry.js
@@ -6,3 +6,32 @@ import tiy from './app.js';
 
 // Looks like the imported module was a function, because here we're executing it!
 tiy();
+
+import Backbone from 'backbone';
+import $ from 'jquery';
+
+const basicView = Backbone.View.extend({
+	tagName: 'section', 
+
+	initialize: function(url, linkName) {
+		this.url = url;
+		this.linkName = linkName;
+	},
+
+	template: function() {
+		//return `hi`;
+		return `<a href="${this.url}"> ${this.LinkName}</a>`
+	
+	},
+
+	render: function () {
+		this.el.innerHTML = this.template();
+		$('body').append(this.el);
+	}
+});
+
+const googleLink = new basicView('http://www.google.com', 'google');
+const tiyLink = new basicView('http://theironyard.com', 'tiy'); 
+googleLink.render(); 
+tiyLink.render(); 
+
